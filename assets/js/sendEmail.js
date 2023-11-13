ab2956e3d65c6598e51d209f584f227dab7ec802
barty-s <117319197+barty-s@users.noreply.github.com>
2023-11-13 15:50:38 +0100

    Implement emailJS integration on contact page

---
+++ assets/js/sendEmail.js
@@ -0,0 +1,18 @@
+function sendMail(contactForm) {
+  emailjs
+    .send("service_ld2eqxa", "contact_form", {
+      from_name: contactForm.name.value,
+      from_email: contactForm.emailaddress.value,
+      project_request: contactForm.projectsummary.value,
+    })
+    .then(
+      function (response) {
+        console.log("SUCCESS", response);
+      },
+      function (error) {
+        console.log("FAILED", error);
+      }
+    );
+
+  return false;
+}
